4a615e8dd204d43eb37bd4510618a2ce08744e66
ethan <justlovediaodiao@outlook.com>
2023-11-14 09:27:11 +0800

    fix lan ip

--- provider/bridge.go
+++ provider/bridge.go
@@ -21,26 +21,24 @@ func startProcess(cmd *exec.Cmd) error {
 }
 
 func lanIP() string {
-	localhost := "127.0.0.1"
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		return localhost
+		return ""
 	}
+	r := "0.0.0.0"
 	for _, address := range addrs {
 		if ipnet, ok := address.(*net.IPNet); ok && ipnet.IP.To4() != nil {
-			// C
-			if ipnet.IP[12] == 192 && ipnet.IP[13] == 168 {
-				return ipnet.IP.String()
-			}
-			// A
-			if ipnet.IP[12] == 10 {
-				return ipnet.IP.String()
-			}
-			// B
-			if ipnet.IP[12] == 172 && ipnet.IP[13] >= 16 && ipnet.IP[13] <= 31 {
-				return ipnet.IP.String()
+			if ipnet.IP[12] == 10 { // A
+				ip := ipnet.IP.String()
+				r = ip
+			} else if ipnet.IP[12] == 172 && ipnet.IP[13] >= 16 && ipnet.IP[13] <= 31 { // B
+				ip := ipnet.IP.String()
+				r = ip
+			} else if ipnet.IP[12] == 192 && ipnet.IP[13] == 168 { // C
+				ip := ipnet.IP.String()
+				return ip // use 192.168.x.x first
 			}
 		}
 	}
-	return localhost
+	return r
 }
